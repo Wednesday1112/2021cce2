bc6cdb454b79504ec6833350ce277421b90a5e36
Wednesday <91170841+Wednesday1112@users.noreply.github.com>
2022-04-07 22:37:55 +0800

    Add files via upload

---
+++ week07/week07-3.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+char line[2000][80];
+int compare(const void *p1,const void *p2)
+{
+	return strcmp((char*)p1,(char*)p2);
+}
+int main()
+{
+	int n;
+	scanf("%d\n",&n);
+	for(int i=0;i<n;i++)
+	{
+		scanf("%s",line[i]); ///只讀國家
+		char other[80];
+		gets(other); ///把名字清掉
+	}                ///讓scanf不會讀到人名
+	qsort(line,n,80,compare);
+	for(int i=0;i<n;i++)
+	{
+		printf("%s\n",line[i]);
+	}
+}
